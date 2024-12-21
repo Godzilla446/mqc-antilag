b6d39c844078a335c1aa66a38afd1ef5fb4779b3
MaximumQc <75767302+MaximumGam@users.noreply.github.com>
2024-12-21 15:10:07 -0500

    Update config.lua
    Updated config

--- config/config.lua
+++ config/config.lua
@@ -1,9 +1,11 @@
 Config = {
-    flameSize = 1.5, -- Default flame size is 1.5, anything above 2.5 looks like shit.
+-------------------------------------------------------------------------------------------------------activate------------------ /antilag
+    flameSize = 2.0, -- Default = 2.0, anything above 2.5 looks like shit.------------------------------------------- /antilagflameSize
+    explosionMaxSpeed = 250, -- Default = 250, Max 1000, this is the max speed of the explosions at high RPM. ---------------------------------- /antilagminspeed
+    explosionMinSpeed = 50, -- Default = 50, Min 50, anything under will crash, this is the min speed of the explosions at low RPM.------------ /antilagmaxspeed
+    basePitch = 1.0, -- Default = 1.0, anything under 0.5 and above 1.0 sounds like shit.--------------------------------- /antilagpitch
 
-    Cars = { "jester", "adder", "a80", "silvia3", "lp700r", "audquattros"}, -- Add cars to the config!
-
-    explosionSpeed = 500, -- I recommend to leave this value at 350. Up to you tho, I cant tell you what to do..
-
-    RPM = 0.85
+    revLimiterRPM = 0.96, -- Default = 0.96, rpm trigger revlimiter antilag 0.9 and 1.0.---------------------------------------- /antilagminrpm
+    
+    RPM = 0.4, -- Default = 0.4, RPM: The amount of RPM needed until the antilag is triggered, 0.1 will trigger the antilag at idle, 0.9 will trigger the antilag ONLY at max RPM.
 }
